653cdd672e9f3507e07293b4a5c7a023e2d20025
dimitris <dimitris@62835d06-2a65-48e8-99eb-22c2268f6e5d>
2009-09-21 21:59:09 +0000

    extracted classes TabPanel
    git-svn-id: svn://www.jabox.org/trunk@44 62835d06-2a65-48e8-99eb-22c2268f6e5d

---
+++ jabox-webapp/src/main/java/org/jabox/webapp/borders/TabPanel1.java
@@ -0,0 +1,22 @@
+package org.jabox.webapp.borders;
+
+import org.apache.wicket.markup.html.panel.Panel;
+/**
+ * Panel representing the content panel for the first tab
+ * 
+ * @author Igor Vaynberg (ivaynberg)
+ * 
+ */
+public class TabPanel1  extends Panel {
+	private static final long serialVersionUID = -2139796431114958720L;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param id
+	 *            component id
+	 */
+	public TabPanel1(String id) {
+		super(id);
+	}
+};
---
+++ jabox-webapp/src/main/java/org/jabox/webapp/borders/TabPanel3.java
@@ -0,0 +1,22 @@
+package org.jabox.webapp.borders;
+
+import org.apache.wicket.markup.html.panel.Panel;
+/**
+ * Panel representing the content panel for the first tab
+ * 
+ * @author Igor Vaynberg (ivaynberg)
+ * 
+ */
+public class TabPanel3  extends Panel {
+	private static final long serialVersionUID = -2139796431114958720L;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param id
+	 *            component id
+	 */
+	public TabPanel3(String id) {
+		super(id);
+	}
+};
