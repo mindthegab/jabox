44c8b31e5da9707ea843e8d8d2672895c15e971f
dimitris <dimitris@62835d06-2a65-48e8-99eb-22c2268f6e5d>
2009-09-28 19:01:03 +0000

    organize imports
    git-svn-id: svn://www.jabox.org/trunk@48 62835d06-2a65-48e8-99eb-22c2268f6e5d

--- bts-jtrac/src/main/java/org/jabox/bts/jtrac/JtracServer.java
+++ bts-jtrac/src/main/java/org/jabox/bts/jtrac/JtracServer.java
@@ -7,7 +7,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URL;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
