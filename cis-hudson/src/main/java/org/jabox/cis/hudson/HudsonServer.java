514750b529cb3972d9828f37c85d5ebf30333c01
dimitris <dimitris@62835d06-2a65-48e8-99eb-22c2268f6e5d>
2009-10-10 14:36:36 +0000

    updated Hudson version to 1.326
    git-svn-id: svn://www.jabox.org/trunk@76 62835d06-2a65-48e8-99eb-22c2268f6e5d

--- cis-hudson/src/main/java/org/jabox/cis/hudson/HudsonServer.java
+++ cis-hudson/src/main/java/org/jabox/cis/hudson/HudsonServer.java
@@ -7,7 +7,7 @@ public class HudsonServer extends AbstractEmbeddedServer {
 
 	private static final String GROUP_ID = "org.jvnet.hudson.main";
 	private static final String ARTIFACT_ID = "hudson-war";
-	private static final String VERSION = "1.322";
+	private static final String VERSION = "1.326";
 	private static final String TYPE = "war";
 
 	@Override
