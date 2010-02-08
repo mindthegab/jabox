52376b49ba74de5cd436870eb6d4326ef5bce55b
dimitris <dimitris@62835d06-2a65-48e8-99eb-22c2268f6e5d>
2010-02-08 00:44:32 +0000

    updated Hudson server to 1.344, in order to fix bug with update center: "Update center gets FormTooLarge error" (http://issues.hudson-ci.org/browse/HUDSON-5210)
    git-svn-id: svn://www.jabox.org/trunk@175 62835d06-2a65-48e8-99eb-22c2268f6e5d

--- cis-hudson/src/main/java/org/jabox/cis/hudson/HudsonServer.java
+++ cis-hudson/src/main/java/org/jabox/cis/hudson/HudsonServer.java
@@ -30,7 +30,7 @@ public class HudsonServer extends AbstractEmbeddedServer {
 
 	private static final String GROUP_ID = "org.jvnet.hudson.main";
 	private static final String ARTIFACT_ID = "hudson-war";
-	private static final String VERSION = "1.341";
+	private static final String VERSION = "1.344";
 	private static final String TYPE = "war";
 
 	@Override
