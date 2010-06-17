54e1f5cab11b3d13dc125200c0695bec29c835b4
dimitris <dimitris@62835d06-2a65-48e8-99eb-22c2268f6e5d>
2010-06-17 19:28:57 +0000

    disable RedmineTest
    git-svn-id: svn://www.jabox.org/trunk@280 62835d06-2a65-48e8-99eb-22c2268f6e5d

--- bts-redmine/src/test/java/org/jabox/its/redmine/RedmineTest.java
+++ bts-redmine/src/test/java/org/jabox/its/redmine/RedmineTest.java
@@ -1,19 +1,19 @@
 package org.jabox.its.redmine;
 
+import junit.framework.TestCase;
+
 import org.jabox.model.Project;
 import org.jabox.model.Server;
 
-import junit.framework.TestCase;
-
-public class RedmineTest extends TestCase {
+public abstract class RedmineTest extends TestCase {
 
 	public void testname() throws Exception {
 		RedmineRepository redmineRepository = new RedmineRepository();
 		Project project = new Project();
 		project.setName("example");
 		RedmineRepositoryConfig config = new RedmineRepositoryConfig();
-		config.password="admin";
-		config.username="admin123";
+		config.password = "admin";
+		config.username = "admin123";
 		Server server = new Server();
 		server.setUrl("http://localhost:8080/");
 		config.setServer(server);
