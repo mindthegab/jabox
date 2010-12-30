56b6401168d93a1657985ac1ecd0dc16d50dd5c6
dimitris <dimitris@62835d06-2a65-48e8-99eb-22c2268f6e5d>
2010-12-30 11:02:15 +0000

    Issue #59
    updated test.
    
    git-svn-id: svn://www.jabox.org/trunk@366 62835d06-2a65-48e8-99eb-22c2268f6e5d

--- scm-git/src/test/java/org/jabox/scm/git/GITRepositoryTest.java
+++ scm-git/src/test/java/org/jabox/scm/git/GITRepositoryTest.java
@@ -1,21 +1,18 @@
 package org.jabox.scm.git;
 
-import junit.framework.Assert;
+import java.io.File;
+
 import junit.framework.TestCase;
 
 public class GITRepositoryTest extends TestCase {
-
-	public void testIsInitialized() {
-		Assert.assertFalse(GITRepository.isInitialized());
-
-	}
 
 	public void testInitialize() {
 		GITRepository.initialize();
 	}
 
-	public void testGetSubversionBaseDir() {
-		fail("Not yet implemented");
+	public void testGetGitBaseDir() {
+		File gitBaseDir = GITRepository.getGitBaseDir();
+		assertTrue(gitBaseDir.exists());
 	}
 
 }
