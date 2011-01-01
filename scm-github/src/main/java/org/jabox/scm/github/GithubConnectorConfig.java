9187b23e23749be5f548b9091bd945bccd2d47ab
dimitris <dimitris@62835d06-2a65-48e8-99eb-22c2268f6e5d>
2011-01-01 15:34:08 +0000

    Issue #59
    updated eclipse config.
    updated GitHub configuration.
    
    git-svn-id: svn://www.jabox.org/trunk@373 62835d06-2a65-48e8-99eb-22c2268f6e5d

--- scm-github/src/main/java/org/jabox/scm/github/GithubConnectorConfig.java
+++ scm-github/src/main/java/org/jabox/scm/github/GithubConnectorConfig.java
@@ -30,14 +30,11 @@ import org.jabox.scm.git.IGITConnectorConfig;
 @DiscriminatorValue(GithubConnector.ID)
 public class GithubConnectorConfig extends DeployerConfig implements
 		IGITConnectorConfig {
-	private static final String GIT = "git://";
-	private static final String HTTPS = "https://";
-
 	private static final long serialVersionUID = -830757629457448866L;
 
-	private static final String GITHUB_COM = "github.com/";
-
-	private static final String GIT_SUFFIX = ".git";
+	private static final String GIT_GITHUB_COM = "git://github.com/";
+	private static final String HTTPS_GITHUB_COM = "https://github.com/";
+	private static final String DOT_GIT = ".git";
 
 	public GithubConnectorConfig() {
 		pluginId = GithubConnector.ID;
@@ -58,18 +55,18 @@ public class GithubConnectorConfig extends DeployerConfig implements
 	@Override
 	public Server getServer() {
 		if (server != null) {
-			server.setUrl(HTTPS + GITHUB_COM + getUsername() + "/");
+			server.setUrl(HTTPS_GITHUB_COM + getUsername() + "/");
 		}
 		return server;
 	}
 
 	public String getScmUrl() {
-		String scmURL = GIT + GITHUB_COM + getUsername() + "/";
+		String scmURL = GIT_GITHUB_COM + getUsername() + "/";
 		return scmURL;
 	}
 
 	public String getProjectScmUrl(String projectName) {
-		return getScmUrl() + "/" + projectName + "/trunk/" + projectName;
+		return getScmUrl() + projectName + DOT_GIT;
 	}
 
 	public String getScmMavenPrefix() {
