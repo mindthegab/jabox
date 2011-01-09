0c88f5211ca045f48a5deb3fb2ab69f2fe55a0b9
dimitris <dimitris@62835d06-2a65-48e8-99eb-22c2268f6e5d>
2011-01-09 12:21:14 +0000

    Issue #84
    Update Hudson to v1.391
    
    git-svn-id: svn://www.jabox.org/trunk@379 62835d06-2a65-48e8-99eb-22c2268f6e5d

--- cis-hudson/src/main/java/org/jabox/cis/hudson/HudsonServer.java
+++ cis-hudson/src/main/java/org/jabox/cis/hudson/HudsonServer.java
@@ -32,7 +32,7 @@ public class HudsonServer extends AbstractEmbeddedServer {
 
 	private static final String GROUP_ID = "org.jvnet.hudson.main";
 	private static final String ARTIFACT_ID = "hudson-war";
-	private static final String VERSION = "1.381";
+	private static final String VERSION = "1.391";
 	private static final String TYPE = "war";
 
 	@Override
