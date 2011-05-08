e3a6d6b599346edbf458294a47cb6b70e38b8b37
dimitris <d.kapanidis@gmail.com>
2011-05-08 17:12:00 +0200

    HudsonServer now inherits AbstractEmdeddedServer as the rest of the servers.

--- cis-hudson/src/main/java/org/jabox/cis/hudson/HudsonServer.java
+++ cis-hudson/src/main/java/org/jabox/cis/hudson/HudsonServer.java
@@ -24,20 +24,20 @@ import java.io.IOException;
 import java.net.URL;
 
 import org.codehaus.plexus.util.FileUtils;
-import org.jabox.apis.embedded.EmbeddedServer;
+import org.jabox.apis.embedded.AbstractEmbeddedServer;
 import org.jabox.environment.Environment;
 import org.jabox.maven.helper.MavenDownloader;
 
-public class HudsonServer implements EmbeddedServer {
-
-	@Override
-	public void startServer() {
-	}
+public class HudsonServer extends AbstractEmbeddedServer {
 
 	private static final String GROUP_ID = "org.jvnet.hudson.main";
 	private static final String ARTIFACT_ID = "hudson-war";
 	private static final String VERSION = "1.395";
 	private static final String TYPE = "war";
+
+	public static void main(final String[] args) throws Exception {
+		new HudsonServer().startServerAndWait();
+	}
 
 	public String getServerName() {
 		return "hudson";
