b227d2995fd3a5a17d4d254be88401a9afd500b0
spiddy <d.kapanidis@gmail.com>
2011-08-31 22:51:43 +0200

    removed old main method of SonarServer

--- sas-sonar/src/main/java/org/jabox/sas/sonar/SonarServer.java
+++ sas-sonar/src/main/java/org/jabox/sas/sonar/SonarServer.java
@@ -183,36 +183,4 @@ public class SonarServer extends AbstractEmbeddedServer {
 
 		}
 	}
-
-	public static void main2(final String[] args) throws Exception {
-		Server server = new Server();
-		SocketConnector connector = new SocketConnector();
-		// Set some timeout options to make debugging easier.
-		connector.setMaxIdleTime(1000 * 60 * 60);
-		connector.setSoLingerTime(-1);
-		connector.setPort(9092);
-		server.setConnectors(new Connector[] { connector });
-
-		WebAppContext bb = new WebAppContext();
-		bb.setServer(server);
-		bb.setContextPath("/");
-		bb
-				.setWar("E:/Documents and Settings/Administrator/.m2/repository/org/codehaus/sonar/sonar-web/1.4.2/sonar-web-1.4.2.war");
-		bb.setParentLoaderPriority(true);
-		server.addHandler(bb);
-
-		try {
-			System.out
-					.println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
-			server.start();
-			while (System.in.available() == 0) {
-				Thread.sleep(5000);
-			}
-			server.stop();
-			server.join();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(100);
-		}
-	}
 }
